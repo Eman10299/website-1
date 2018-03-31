27906ee66c2ec1779deca1784d82a2d2f2103164
Eman10299 <37423053+Eman10299@users.noreply.github.com>
2018-03-31 11:46:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function revealMessage() {
 
 document.getElementById("hiddenMessage").style.display = 'block';
 }
+
+function countDown() {
+  var currentVal = document.getElementById("countDownButton").innerHTML;
+  var newVal = currentVal -1;
+  document.getElementById("countdownButton").innerHTML = newVal;
